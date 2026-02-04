3de8a7513bc1e9d446095facfa1d93262c390824
huahua <1494133104@qq.com>
2026-02-04 16:15:13 +0800

    feat: add localhost configuration and update deployment script for local testing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -31,6 +31,12 @@ export default defineConfig({
       type: "edr-simulated",
       chainType: "op",
     },
+    localhost: {
+      type: "http",
+      chainType: "l1",
+      url: "http://127.0.0.1:8545",
+      accounts: [configVariable("LOCALHOST_PRIVATE_KEY")],
+    },
     sepolia: {
       type: "http",
       chainType: "l1",
